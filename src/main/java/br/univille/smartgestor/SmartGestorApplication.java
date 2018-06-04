84f3155efc1e016c36c5dfc2cfe8eeb7f7873e6b
gabrielmeneghel <gabriel.meneghel@univille.br>
2018-06-04 19:44:15 -0300

    first commit

---
+++ src/main/java/br/univille/smartgestor/SmartGestorApplication.java
@@ -0,0 +1,12 @@
+package br.univille.smartgestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGestorApplication.class, args);
+	}
+}
